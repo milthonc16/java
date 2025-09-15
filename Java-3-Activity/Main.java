8e08714752fdeeb12062f337230cef0f48205b5b
milthonc16 <milthonc@nycstudents.net>
2025-09-15 16:13:33 +0000

    jj

---
+++ Java-3-Activity/Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("Hi");
+
+   System.out.println("Milthon Casas");
+
+   System.out.println("Milthon casas. I live on bell blvd, bayside");
+
+   System.out.println("Milthon casas.\n I live on bell blvd, bayside");
+    
+  }
+
+  
+}
---
+++ Java-3b-CW/Java-3b-CW/Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  // This is a single line comment in Java
+  // Multi-line comments are enclosed within  /*  and  */
+  
+  /* 
+  Init function which runs when 
+  the program begins is invoked(called) from within the Main function.
+  */
+  void init(){
+    // print Welcome
+    
+
+    // print Hello with the quotes displayed
+    
+    
+    // print \Java Code\ as shown
+    
+    
+    // print the name Joe tabbed once
+    
+
+    // print the name Mary tabbed twice
+    
+    
+    // print one, two and three on separate lines using a single line of code
+      
+	
+  }
+  
+}
