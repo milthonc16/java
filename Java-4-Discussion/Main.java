6acd0abbad260036f52f8347d9537f35773e31c2
milthonc16 <milthonc@nycstudents.net>
2025-09-25 14:11:29 +0000

    java

---
+++ Java-4-Discussion/Main.java
@@ -0,0 +1,26 @@
+class Main {
+  
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function runs when the program begins.  
+
+  void init(){
+    // Number of cars
+       int numOfCars = 20;
+    // Investment amount in currency
+      float invstmtAmt = 200.50;
+    // Person's first name
+      string Fname = "judith";
+    // Is a game over?
+      bolean isGameOver = false;
+    // Store the first initial of a name
+      char fInitial = 'N';
+    // Maximum number of customers in a theater (declare as a constant)
+      final int MaxCust = 500;
+
+  }
+
+  
+}
