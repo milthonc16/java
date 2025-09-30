ddc1897f1e92501fd1cbfc4858aeab405aca5f3a
milthonc16 <milthonc@nycstudents.net>
2025-09-30 13:43:36 +0000

    update

---
+++ Lesson-08-Activity/Main.java
@@ -0,0 +1,21 @@
+class Main {
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+		print("hello");
+		double cel = F to C(73.6);
+  }
+  
+	void print(String v){
+		System.out.println(v);
+	}
+
+	FtoC(double F){
+		double c = (f-32)*9/5;
+		return c;
+	}
+ 
+}
